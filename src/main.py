9ebca0bc1a7e5a25aab2b8fc08142c2a5c9d5651
KandalintsevGames <130852454+KandalintsevGames@users.noreply.github.com>
2025-05-14 10:27:03 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 import pygame
-from pygame.locals import *
 
 pygame.init()
-
-screen = pygame.display.set_mode((1920,1080))
+x = 1920
+y= 1080
+screen = pygame.display.set_mode((x,y))
 
 player = pygame.image.load("player.gif")
 player_rect = player.get_rect()
+player_rect.y = y/2
+player_rect.x = x/2
 running = True
 while running:
     screen.fill("white")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == KEYDOWN:
-            player_rect.y += 5
+        elif event.type == pygame.KEYDOWN:
 
+            if event.key == pygame.K_w:
+                player_rect.y -= 5
+            elif event.key == pygame.K_s:
+                player_rect.y += 5
+            elif event.key == pygame.K_a:
+                player_rect.x -= 5
+            elif event.key == pygame.K_d:
+                player_rect.x += 5
     screen.blit(player,player_rect)
     pygame.display.update()
 pygame.quit()
