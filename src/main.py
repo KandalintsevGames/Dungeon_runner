1fe4764d26f872212e264468a1e4f9b3e2cae395
KandalintsevGames <130852454+KandalintsevGames@users.noreply.github.com>
2025-05-16 10:21:22 +0200

    Global Player code changes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 import pygame
+import player
 
 pygame.init()
 x = 1920
 y= 1080
 screen = pygame.display.set_mode((x,y))
 
-player = pygame.image.load("player.gif")
-player_rect = player.get_rect()
-player_rect.y = y/2
-player_rect.x = x/2
 running = True
 while running:
     screen.fill("white")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-       
+        player.movement()
+    screen.blit(player.player,player.player_rect)
+
     pygame.display.update()
 pygame.quit()
--- src/player.py
+++ src/player.py
@@ -2,6 +2,18 @@ import pygame
 from pygame.locals import *
 
 player_image_loaction = "src/assets/player.gif"
-
 player = pygame.image.load(player_image_loaction)
 player_rect = player.get_rect()
+
+def movement():
+    keys =pygame.key.get_pressed()
+    if keys[K_w]:
+        player_rect.y -= 5                
+    if keys[K_s]:
+        player_rect.y += 5                
+    if keys[K_a]:
+        player_rect.x -= 5                
+    if keys[K_d]:
+        player_rect.x += 5                
+        
+
