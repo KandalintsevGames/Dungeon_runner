2aff70155549f6b06e4578775d75cb99b4cc238f
KandalintsevGames <130852454+KandalintsevGames@users.noreply.github.com>
2025-05-21 08:15:49 +0200

    one more update
    https://prod.liveshare.vsengsaas.visualstudio.com/join?3E802927391D7D3F91B11F6A7869B2A9F276

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import pygame
 import player
-import enemy
 pygame.init()
 x = 1920
 y= 1080
@@ -8,14 +7,11 @@ screen = pygame.display.set_mode((x,y))
 
 running = True
 while running:
-    liste = enemy.rechner_enemy_goto(enemy.enemy_rect,player.player_rect)
-    enemy.enemy_move(liste)
     screen.fill("white")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         player.movement()
     screen.blit(player.player,player.player_rect)
-    screen.blit(enemy.enemy,enemy.enemy_rect)
     pygame.display.update()
 pygame.quit()
