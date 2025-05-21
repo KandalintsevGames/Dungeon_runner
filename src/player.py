95f04f603315a78f71cc3c73a04e7ffd56d1a50b
KandalintsevGames <130852454+KandalintsevGames@users.noreply.github.com>
2025-05-21 08:14:29 +0200

    adding player animation file

--- src/player.py
+++ src/player.py
@@ -1,19 +1,26 @@
 import pygame
+import time
 from pygame.locals import *
 
-player_image_loaction = "src/assets/player.png"
-player = pygame.image.load(player_image_loaction)
+player_size = (100,100)
+
+player = pygame.image.load("src/assets/player_movement/movement1.png")
+player = pygame.transform.scale(player,player_size)
 player_rect = player.get_rect()
 
+  
+
+
 def movement():
+    speed = 4
     keys =pygame.key.get_pressed()
     if keys[K_w]:
-        player_rect.y -= 1                
+        player_rect.y -= speed                
     if keys[K_s]:
-        player_rect.y += 1                
+        player_rect.y += speed                
     if keys[K_a]:
-        player_rect.x -= 1                
+        player_rect.x -= speed                
     if keys[K_d]:
-        player_rect.x += 1                
-        
+        player_rect.x += speed                
+            
 
