0c8587dd7754ac166c5537dee3cdd073fa00cbc5
KandalintsevGames <130852454+KandalintsevGames@users.noreply.github.com>
2025-05-28 09:43:52 +0200

    Revert "i do not know"
    This reverts commit 6627b984551aced574bc6f0755c027d6b72ecdf2, reversing
    changes made to bc11ef02e5d37c06eb57fc01043cbcfa19e16c2b.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ import pygame
 import player
 from enemy import init_enemy,enemy_goto
 pygame.init()
-backgrount = pygame.image.load("src/assets/background.png")
+background = pygame.image.load("src/assets/background.png")
 x = 1920
 y= 1080
 screen = pygame.display.set_mode((x,y))
-backgrount_img = pygame.transform.scale(backgrount,(x,y))
+background_img = pygame.transform.scale(background,(x,y))
+
 def game_loop():
     FPS = 60
     clock = pygame.time.Clock()
@@ -19,17 +20,18 @@ def game_loop():
     player_img , player_rect = player.player_init()
     player_life = 100
     while running:
-        screen.blit(backgrount_img,(0,0))
-        #screen.fill("white")
+        
+        screen.fill("white")
+        screen.blit(background_img,(0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                enemy_rect_dictionary= player.Blitzi(enemy_rect_dictionary,amount_enemy)
+        enemy_rect_dictionary= player.Blitzi(enemy_rect_dictionary,amount_enemy)
         player.movement(player_rect)
+
         screen.blit(player_img,player_rect)
 
-
+        fps = clock.get_fps()
         
 
         #enemy.enemy_rect = enemy.enemy.get_rect(center = (liste_enemy_movement[0],liste_enemy_movement[1]))
@@ -44,10 +46,9 @@ def game_loop():
                 screen.blit(enemy_img,enemy_rect_dictionary[i][0])
                 screen.blit(enemy_life_black,(enemy_rect_dictionary[i][0].x,enemy_rect_dictionary[i][0].y -30))
                 screen.blit(enemy_life_red,(int(enemy_rect_dictionary[i][0].x),int(enemy_rect_dictionary[i][0].y-30)))
-
         pygame.display.update()
         clock.tick(FPS)
-        print(clock.tick(FPS))
+        print(fps)
     pygame.quit()
 if __name__ == "__main__":
     game_loop()
